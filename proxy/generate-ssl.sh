91ff2b88666fb267974d97deb8464381ad78b7b4
TienZe <hertyv72@gmail.com>
2025-06-07 01:44:55 +0700

    Update script generate ssl

--- proxy/generate-ssl.sh
+++ proxy/generate-ssl.sh
@@ -1,7 +1,16 @@
+# Generate SSL certificate
 docker exec -it toeic_proxy bash -c "certbot certonly --webroot \
   -w /var/www/certbot \
   -d toeicbooster.io.vn \
   -d www.toeicbooster.io.vn \
   --email hertyv27@gmail.com \
   --agree-tos \
-  --no-eff-email"
+  --no-eff-email \
+  -v"
+
+
+# Copy actual SSL certificate to ssl folder inside toeic_proxy container
+docker exec -it toeic_proxy bash -c 'cd /etc/letsencrypt/live/toeicbooster.io.vn && cp -L --remove-destination cert.pem temp && mv temp cert.pem'
+docker exec -it toeic_proxy bash -c 'cd /etc/letsencrypt/live/toeicbooster.io.vn && cp -L --remove-destination chain.pem temp && mv temp chain.pem'
+docker exec -it toeic_proxy bash -c 'cd /etc/letsencrypt/live/toeicbooster.io.vn && cp -L --remove-destination fullchain.pem temp && mv temp fullchain.pem'
+docker exec -it toeic_proxy bash -c 'cd /etc/letsencrypt/live/toeicbooster.io.vn && cp -L --remove-destination privkey.pem temp && mv temp privkey.pem'
---
+++ proxy/renew-ssl.sh
@@ -0,0 +1,1 @@
+docker exec -it toeic_proxy bash -c "certbot renew"
